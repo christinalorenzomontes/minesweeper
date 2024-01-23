3f08dc055cd6e3815297f31fd0315a02d4db27e6
Christina Lorenzo <christinalorenzo@gmail.com>
2024-01-23 13:43:21 +0100

    add funcionalities

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid');
     const flagsLeft = document.querySelector('#flags-left');
     const width = 10;
+    let squares = [];
     let bombAmount = 20;
 
     // Create board
@@ -19,8 +20,41 @@ document.addEventListener('DOMContentLoaded', () => {
             square.id = i;
             square.classList.add(shuffledArray[i]);
             grid.appendChild(square);
+            squares.push(square);
+
+            // Normal Click
+            square.addEventListener('click', () => {
+                click(square);
+            })
+
+            // Control and Left Click
+            square.addEventListener('click', () => {
+                // addFlag(square);
+            })
+        }
+
+        // Add numbers
+        for ( let i = 0; i < squares.length; i++) {
+            let total = 0;
+            const isLeftEdge = (i % width === 0);
+            const isRightEdge = (i % width === width - 1);
+
+            if (squares[i].classList.contains('valid') ) {
+                if ( i > 0 && !isLeftEdge && squares[i - 1].classList.contains('bomb') ) total++
+                if ( i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains('bomb') ) total++
+                if ( i > 10 && squares[i - width].classList.contains('bomb') ) total++
+                if ( i > 11 && !isLeftEdge && squares[i - width - 1].classList.contains('bomb') ) total++
+            }
         }
     }
     createBoard();
+
+    const click = (square) => {
+        console.log(square);
+    }
+
+    // const addFlag = (square) => {
+    //     console.log(square);
+    // }
 })
 
